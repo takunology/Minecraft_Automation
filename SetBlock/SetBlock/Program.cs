7712e051fd38ae637c376dc9cb1ab65b3f90dacc
takunology <m15025@g.metro-cit.ac.jp>
2019-08-25 15:36:30 +0900

    SetBlock Command in CSharp

---
+++ SetBlock/SetBlock/Program.cs
@@ -0,0 +1,90 @@
+﻿using CoreRCON;
+using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Now Starting...");
+            await minecraft();
+        }
+
+        static async Task minecraft()
+        {
+            var serveraddress = IPAddress.Parse("127.0.0.1"); //IPアドレスとして扱うための変換
+            var serverpass = "minecraft"; //RCONでログインするためのパスワード
+            ushort port = 25575; //サーバのポート番号
+
+            string Player_Name = "PlayerName";
+            string TPCommand = "/tp " + Player_Name + " ~ ~ ~"; //プレイヤーの位置情報を得るためのコマンド
+
+            //CoreRCONを使ってMinecraftへ接続
+            try
+            {
+                var connection = new RCON(serveraddress, port, serverpass);
+                var result = await connection.SendCommandAsync(TPCommand);
+
+                string GetNum = Regex.Replace(result, @"[^0-9,.]", ""); //座標数値のみ検索
+                string[] StrArray = GetNum.Split(','); //カンマで区切られた文字を部分的に取り出して、配列に保持
+
+                Console.WriteLine(result);//生のデータ
+                Console.WriteLine(GetNum + "\n");//正規表現にてフィルタされた値の確認
+
+                foreach(string str in StrArray)
+                {
+                    Console.WriteLine(str);//配列で保持された座標データの確認（上からx, y, z座標）
+                }
+
+                double Position_x = double.Parse(StrArray[0]); //南北方向 北へ進むほど数値が小さくなる
+                double Position_y = double.Parse(StrArray[1]); //高さ
+                double Position_z = double.Parse(StrArray[2]); //東西方向 東へ進むほど数値が大きくなる
+
+                double x = Position_x, y = Position_y, z = Position_z; //配置するために使用する座標
+
+                //ブロック名(grass:草ブロック, glass:ガラスブロック etc...)
+                string Block_Name = "stone";
+                //ブロックを置くコマンド(1個だけ配置)
+                //string SetBlock = "/setblock " + Position_x.ToString() + " " + Position_y.ToString() + " " + Position_z.ToString() + " " + Block_Name;
+                
+                // 線形(x方向)にブロックを配置する
+                /*for(int i = 0; i < 5; i++)
+                {
+                    x++;
+                    string SetBlock = "/setblock " + x.ToString() + " " + y.ToString() + " " + z.ToString() + " " + Block_Name;
+
+                    //コマンド送信
+                    result = await connection.SendCommandAsync(SetBlock);
+                    Console.WriteLine(result);
+                }*/
+
+                //2次元方向(x, z方向)にブロックを配置する
+                for (int i = 0; i < 5; i++)
+                {
+                    x++;
+                    for (int j = 0; j < 5; j++)
+                    {
+                        z++;
+                        string SetBlock = "/setblock " + x.ToString() + " " + y.ToString() + " " + z.ToString() + " " + Block_Name;
+                        result = await connection.SendCommandAsync(SetBlock);
+                        Console.WriteLine(result);
+                    }
+                    z = Position_z; //位置情報を修正(初期化)
+                }
+                Console.ForegroundColor = ConsoleColor.Green; //コンソール文字列に色付け
+                Console.WriteLine("\nDone."); //ちょっとカッコつけて完了の表示
+                Console.ForegroundColor = ConsoleColor.White; //色を戻す
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
--- SimpleCommand_Python/main.py
+++ SimpleCommand_Python/main.py
@@ -4,5 +4,5 @@ server_pass = "minecraft"
 from mcrcon import MCRcon
 
 with MCRcon(server_address, server_pass, 25575)as mcr:
-    mcr.command("/time set 0")
-    mcr.command("/weather clear")
+    mcr.command("/time set night")
+    mcr.command("/weather rain")
