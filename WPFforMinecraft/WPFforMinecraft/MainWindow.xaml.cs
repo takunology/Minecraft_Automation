365beaa732ed39ba3f4fcba634db28ec746611e7
takunology <m15025@g.metro-cit.ac.jp>
2019-10-22 02:08:04 +0900

    非同期処理の実装（未完成）

--- WPFforMinecraft/WPFforMinecraft/MainWindow.xaml.cs
+++ WPFforMinecraft/WPFforMinecraft/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CoreRCON;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +27,42 @@ namespace WPFforMinecraft
         {
             InitializeComponent();
             MouseLeftButtonDown += (o, e) => DragMove();
+
         }
+
+        
+        public async Task Command_KeyDown(object sender, KeyEventArgs e)
+        {
+            SendCommand sendCommand = new SendCommand();
+            if (e.Key == Key.Enter)
+            {
+                //Send(Command.Text);
+                await Send();
+                
+            }
+        }
+
+        static async Task Send()
+        {
+            var ServerAddress = IPAddress.Parse("127.0.0.1");
+            var ServerPass = "minecraft";
+            ushort ServerPort = 25575;
+            var mainWindow = (MainWindow)App.Current.MainWindow;
+
+            try
+            {
+                var Connection = new RCON(ServerAddress, ServerPort, ServerPass);
+                var Result = await Connection.SendCommandAsync("/time set 0");
+                mainWindow.ConsoleLog.Text = Result.ToString();
+                //await Task.Delay(100);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                mainWindow.ConsoleLog.Text = e.ToString();
+            }
+        }
+
     }
 }
---
+++ WPFforMinecraft/WPFforMinecraft/SendCommand.cs
@@ -0,0 +1,16 @@
+﻿using CoreRCON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFforMinecraft
+{
+    public class SendCommand
+    {
+        
+    }
+}
