19faa5394f1a3df614532c5d18276b45b708f5ae
takunology <m15025@g.metro-cit.ac.jp>
2019-11-14 20:21:06 +0900

    マイクラコマンドに対応

--- MazeGenerator/MazeGenerator/Program.cs
+++ MazeGenerator/MazeGenerator/Program.cs
@@ -1,19 +1,97 @@
-﻿using System;
+﻿using CoreRCON;
+using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MazeGenerator
 {
     class Program
     {
-        static string[,] Maze = new string[0, 0];
+        static string[,] Maze = new string[0, 0]; //迷路データ
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Input(); //入力
             WallGenerate(); //壁生成
             Generate(); //迷路生成
             Check(); //修正
-            //ShowMaze(); //最終完成図
-            Console.WriteLine("迷路完成！");
+            ShowMaze();
+            Console.WriteLine("この迷路を生成します");
+            Console.ReadKey();
+            await MinecraftAutomation();
+            Console.WriteLine("Done.");
+        }
+
+        static async Task MinecraftAutomation()
+        {
+            var ServerAddrss = IPAddress.Parse("127.0.0.1");
+            var ServerPass = "minecraft";
+            ushort Port = 25575;
+            var Connection = new RCON(ServerAddrss, Port, ServerPass);
+
+            string PlayerName = "Prau_Splacraft";
+            string TPCommand = "/tp " + PlayerName + " ~ ~ ~"; //プレイヤーの位置情報を得るためのコマンド
+            var result = await Connection.SendCommandAsync(TPCommand);
+            
+            string GetNum = Regex.Replace(result, @"[^0-9-,.]", ""); //座標数値のみ検索
+            string[] StrArray = GetNum.Split(','); //カンマで区切られた文字を部分的に取り出して、配列に保持
+            double Position_x = double.Parse(StrArray[0]); //南北方向 北へ進むほど数値が小さくなる
+            double Position_y = double.Parse(StrArray[1]); //高さ
+            double Position_z = double.Parse(StrArray[2]); //東西方向 東へ進むほど数値が大きくなる
+
+            ShowRange("x", Position_x, Maze.GetLength(0));
+            ShowRange("z", Position_z, Maze.GetLength(1));
+
+
+            Position_y = 63;
+            //地面生成
+            for (int x = 0; x < (Maze.GetLength(0)); x++)
+            {
+                for (int z = 0; z < (Maze.GetLength(1)); z++)
+                {
+                    string SetBlock = "/setblock " + (Position_x + x).ToString() + " " + Position_y.ToString() + " " + (Position_z + z).ToString() + " " + "stone";
+                    result = await Connection.SendCommandAsync(SetBlock);
+                    Console.WriteLine(result);
+                    await Task.Delay(10);
+                }
+            }
+
+            
+            Position_y++; //地面から1マス上で迷路を作る
+
+            for (int x = 0; x < (Maze.GetLength(0)); x++)
+            {
+                for(int z = 0; z < (Maze.GetLength(1)); z++)
+                {
+                    if(Maze[x, z] == "■")
+                    {
+                        //Connection = new RCON(ServerAddrss, Port, ServerPass);
+                        string SetBlock = "/setblock " + (Position_x + x).ToString() + " " + Position_y.ToString() + " " + (Position_z + z).ToString() + " " + "grass";                       
+                        result = await Connection.SendCommandAsync(SetBlock);
+                        Console.WriteLine(result);
+                        await Task.Delay(10); //遅延自体も await 修飾子が必要！
+                    }
+                    else
+                    {
+                        string SetBlock = "/setblock " + (Position_x + x).ToString() + " " + Position_y.ToString() + " " + (Position_z + z).ToString() + " " + "air";
+                        result = await Connection.SendCommandAsync(SetBlock);
+                        Console.WriteLine(result);
+                        await Task.Delay(10); //遅延自体も await 修飾子が必要！
+                    }
+                }
+            }
+            
+        }
+
+        static void ShowRange(string Vector, double Pos, int Input)
+        {
+            int Position = (int)Pos;
+            Console.Write(Vector + "= {0} to ", Position); // 現在の座標
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}", Position + Input); // 範囲指定後の座標
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         static void Input()
@@ -22,10 +100,10 @@ namespace MazeGenerator
             {
                 Console.Write("X：");
                 int X = int.Parse(Console.ReadLine());
-                //int X = 21;
+                //int X = 31;
                 Console.Write("Z：");
                 int Y = int.Parse(Console.ReadLine());
-                //int Y = 21;
+                //int Y = 31;
 
                 if (X % 2 == 1 && Y % 2 == 1)
                 {
@@ -219,9 +297,9 @@ namespace MazeGenerator
                         break;
                 }
                 Console.Clear();
-                Console.WriteLine($"X:{PositionX} Y:{PositionY} 方向:{direction}");
+                //Console.WriteLine($"X:{PositionX} Y:{PositionY} 方向:{direction}");
                 ShowMaze();
-                //Thread.Sleep(5);
+                //Thread.Sleep(10);
                 //Console.ReadKey();
             }
         }
@@ -262,10 +340,10 @@ namespace MazeGenerator
                             }
                         }
                     }
-                    Console.WriteLine("修正中");
+                    //Console.WriteLine("修正中");
                     Console.Clear();
                     ShowMaze();
-                    //Thread.Sleep(5);
+                    //Thread.Sleep(10);
                 }
             }
         }
