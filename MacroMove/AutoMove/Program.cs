07331f1c0cd62d544bcb3bee7051415d39e2854b
takunology <m15025@g.metro-cit.ac.jp>
2020-03-09 23:58:01 +0900

    プレイヤーの自動移動

---
+++ MacroMove/AutoMove/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.ComponentModel;
+
+namespace AutoMove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SendToMinecraft();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void SendToMinecraft()
+        {
+            //マイクラのプロセス
+            var process = Process.GetProcessesByName("javaw");
+
+            SetForegroundWindow(process[0].MainWindowHandle);
+            Console.WriteLine("Press W key...");
+            SendKeys.SendWait("{W 1000}");
+            Thread.Sleep(500);
+            Console.WriteLine("Press S key...");
+            SendKeys.SendWait("{S 1000}");
+            Console.WriteLine("Press D key...");
+            SendKeys.SendWait("{D 1000}");
+            Thread.Sleep(500);
+            Console.WriteLine("Press A key");
+            SendKeys.SendWait("{A 2000}");
+        }
+    }
+}
---
+++ MacroMove/MacroMove/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessGetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get Process...");
+            Processes();
+            GetMinecraftProcess();
+        }
+
+        public static void GetMinecraftProcess()
+        {
+            //マイクラのプロセス名が "javaw" だったので詳細を得る
+            Process[] process = Process.GetProcessesByName("javaw");
+
+            Console.WriteLine($"プロセスID : {process[0].Id}");
+            Console.WriteLine($"プロセス名 : {process[0].ProcessName}");
+            Console.WriteLine($"ウィンドウタイトル : {process[0].MainWindowTitle}");
+            Console.WriteLine($"メインモジュール : {process[0].MainModule}");
+            Console.WriteLine($"プロセスの優先順位 : {process[0].BasePriority}");
+            Console.WriteLine($"順位の種類 : {process[0].PriorityClass}");
+            Console.WriteLine($"メモリ割り当てサイズ : {process[0].PagedMemorySize64}");
+            Console.WriteLine($"仮想メモリサイズ : {process[0].VirtualMemorySize64}");
+        }
+
+        public static void Processes()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                Console.WriteLine($"{process.Id}:{process.ProcessName}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
